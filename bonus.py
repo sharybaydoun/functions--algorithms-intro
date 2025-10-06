621b4ca2fa744df155421488eda7322b5df33026
sharybaydoun <Sharibaydoun60@gmail.com>
2025-10-06 16:17:40 +0300

    Creating fibonacci sequence recursive approach with user input and print both approaches

--- bonus.py
+++ bonus.py
@@ -1,4 +1,4 @@
-def Fibonacci_sequence(n):
+def Fibonacci_sequence(n): # iterative approach 
    
     if n < 0:
         return "Input must be a non-negative integer"
@@ -7,16 +7,30 @@ def Fibonacci_sequence(n):
     elif n==1:
         return[0,1]
     sequence=[0,1]
-    for i in range(2, n):
+    for i in range(2, n): # loop runs n-2 times so O(n)
         next_number = sequence[i-1] + sequence[i-2]
         sequence.append(next_number)
     
     return sequence
     
-num=int(input("Enter a positive integer:"))
-print("Fibonacci sequence up to",num,":",Fibonacci_sequence(num))
 
+def fibonacci_sequence_recursive(n):
+    if n < 0:
+        return "Input must be a non-negative integer"
+    if n == 0:
+        return [0]
+    if n == 1:
+        return [0, 1] 
     
-   
+    seq = fibonacci_sequence_recursive(n-1)
+    
+    if len(seq) < n:
+        seq.append(seq[-1] + seq[-2])
+    
+    return seq
+
+num=int(input("Enter a positive integer:"))
+print("Fibonacci sequence iterative approach up to",num,":",Fibonacci_sequence(num))
+print("Fibonacci sequence recursive approach up to",num,":",fibonacci_sequence_recursive(num))
 
     
